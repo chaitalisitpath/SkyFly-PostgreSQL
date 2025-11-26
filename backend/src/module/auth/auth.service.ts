ff9a9f8ff223d00bebbd89c427a408f79a3d9216
Chaitali <chaitalis.itpath@gmail.com>
2025-11-26 15:56:52 +0530

    change

--- backend/src/module/auth/auth.service.ts
+++ backend/src/module/auth/auth.service.ts
@@ -1,4 +1,21 @@
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        name: dto.name,
+        email: dto.email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
