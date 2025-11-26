9c254e15e584292c57bc5ac0d71fe6a41214da45
Chaitali <chaitalis.itpath@gmail.com>
2025-11-26 18:14:47 +0530

    user register

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from './module/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, PrismaModule],
+  imports: [DatabaseModule, PrismaModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- backend/src/module/auth/auth.service.ts
+++ backend/src/module/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import * as bcrypt from 'bcrypt';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from '../../../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import { Injectable } from '@nestjs/common';
 
