f8e3bedd782c037ef705d657e4175e6157c7f73b
Akshat Solanki <akshatsolanki239@gmail.com>
2025-12-11 22:03:25 +0530

    change

--- backend/src/module/aircraft/aircraft.service.ts
+++ backend/src/module/aircraft/aircraft.service.ts
@@ -7,7 +7,7 @@ import { Prisma } from '@prisma/client';
 export class AircraftService {
   constructor(private prisma: PrismaService) {}
 
-  // Get all aircraft
+  // Get all aircrafts
   async getAllAircraft() {
     return this.prisma.aircraft.findMany({
       include: {
